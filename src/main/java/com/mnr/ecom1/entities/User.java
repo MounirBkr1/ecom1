0a83fc3e612de3fad7bc8039102227d645b0bf46
Mounir Bkr <miirbri1@gmail.com>
2024-07-15 11:35:29 +0100

    entities

---
+++ src/main/java/com/mnr/ecom1/entities/User.java
@@ -0,0 +1,28 @@
+package com.mnr.ecom1.entities;
+
+
+import com.mnr.ecom1.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.context.annotation.Configuration;
+
+@Entity
+@Data
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public String email;
+    private String password;
+    private String name;
+    //ENUMERATION
+    private UserRole role;
+
+    @Lob //help us to map large binary objects or large character objects to a specific entity in our database
+    //@Column(columnDefinition = "longLob")
+    private byte[] img;
+
+}
