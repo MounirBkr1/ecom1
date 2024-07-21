a59514d1541ed9842d3817a519d31cacd41a7347
MounirBkr1 <miirbri1@gmail.com>
2024-07-21 07:01:03 -0400

    correction1

--- src/main/java/com/mnr/ecom1/config/SimpleCorsFilter.java
+++ src/main/java/com/mnr/ecom1/config/SimpleCorsFilter.java
@@ -32,7 +32,7 @@ public class SimpleCorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
         throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        HttpServletRequest request = (HttpServletRequest) res;
+        HttpServletRequest request = (HttpServletRequest) req;
         Map<String, String> map = new HashMap<>();
 
         String originHeader = request.getHeader("origin");
